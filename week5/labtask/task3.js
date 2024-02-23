808aeca6037535c71ffb38ce09a9bccf2f85806a
UmeFarva <119131203+UmeFarva@users.noreply.github.com>
2024-02-23 20:42:01 +0500

    Add files via upload

---
+++ week5/labtask/task3.js
@@ -0,0 +1,11 @@
+
+ function applyFunction(array) {
+    const result = array.map(function(item) {
+        return item * 2;
+    });
+    return result; // Return the result of map operation
+}
+
+const numbers = [1, 2, 3];
+const result1 = applyFunction(numbers);
+console.log(result1);
