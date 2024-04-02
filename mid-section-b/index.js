19eab0733a03aa2bc70a706acabc7b36ee8bf0fa
UmeFarva <119131203+UmeFarva@users.noreply.github.com>
2024-04-02 14:55:05 +0500

    Add files via upload

---
+++ mid-section-b/index.js
@@ -0,0 +1,27 @@
+const prev=document.getElementById("prev");
+const next=document.getElementById("next");
+const div1=document.getElementById("div");
+const source=document.getElementById("img1");
+const source1=document.getElementById("img2");
+const sourc2e=document.getElementById("img3");
+
+function previous(){
+      
+      const div=document.createElement("div")
+      div.className="container";
+      div.textContent=source;
+    
+
+
+}
+function next(){
+      
+    const div=document.createElement("div")
+    div.className="container";
+    div.textContent=so;
+  
+
+
+}
+prev=onclick=previous;
+prev=onclick=next;
